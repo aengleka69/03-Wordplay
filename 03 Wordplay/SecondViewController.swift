3c087af9ba88cf5d9d16f7a8aac520b210dd3d12
andrewdonahue <donahue00@gmail.com>
2019-03-26 13:13:05 -0400

    nil

--- 03 Wordplay/SecondViewController.swift
+++ 03 Wordplay/SecondViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController
 {
-
+    var character = ""
+    
     override func viewDidLoad()
     
     {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
         
     }
     
